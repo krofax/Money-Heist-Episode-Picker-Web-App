2b7f56dc41249fc6bd3ea170eed936fafc0f6fe0
Blessing Krofegha <bkrofegha@gmail.com>
2020-04-16 18:12:12 +0200

    refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import React, { useContext, Fragment } from 'react'
-import { Store } from './Store'
-import { Link } from '@reach/router'
+import React, { Fragment } from 'react'
 
-export default function App({
+ const App = ({
   children
 }: {
   children: JSX.Element
-}): JSX.Element {
-  const { state } = useContext(Store)
+}): JSX.Element => {
 
   return (
     <Fragment>
@@ -16,12 +13,10 @@ export default function App({
           <h1>Money Heist</h1>
           <p>Pick your favourite episode😘</p>
         </div>
-        <div>
-          <Link to='/'>Home</Link>
-          <Link to='/faves'>Favourite(s): {state.favourites.length}</Link>
-        </div>
       </header>
       {children}
     </Fragment>
   )
-}
+ }
+
+export default App
