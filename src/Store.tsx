8a9f5abad25b5ca8d19320ec3326712f71342ce7
Blessing Krofegha <bkrofegha@gmail.com>
2020-04-17 00:48:02 +0200

    fetched episodes from endpoints

--- src/Store.tsx
+++ src/Store.tsx
@@ -1,26 +1,18 @@
 import React, { useReducer, createContext } from 'react'
 import { IState, IAction } from './types/interfaces'
-
 const initialState: IState = {
   episodes: [],
   favourites: []
 }
-
 export const Store = createContext<IState | any>(initialState)
-
 const reducer = (state: IState, action: IAction): IState => {
   switch (action.type) {
     case 'FETCH_DATA':
       return { ...state, episodes: action.payload }
-    case 'ADD_FAV':
-      return { ...state, favourites: [...state.favourites, action.payload] }
-    case 'REMOVE_FAV':
-      return { ...state, favourites: action.payload }
     default:
       return state
   }
 }
-
 export const StoreProvider = ({ children }: JSX.ElementChildrenAttribute): JSX.Element => {
   const [state, dispatch] = useReducer(reducer, initialState)
   return <Store.Provider value={{ state, dispatch }}>{children}</Store.Provider>
--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,11 @@ import ReactDOM from 'react-dom'
 
 import './index.css'
 import { StoreProvider } from './Store'
-import { Router, RouteComponentProps } from '@reach/router'
 import HomePage from './components/HomePage'
-
-const RouterPage = (
-  props: { pageComponent: JSX.Element } & RouteComponentProps
-) => props.pageComponent
-
 
 ReactDOM.render(
   <StoreProvider>
-    <Router>
-      <RouterPage pageComponent={<HomePage />} path='/' />
-    </Router>
+      <HomePage /> 
   </StoreProvider>,
   document.getElementById('root')
 )
